280585b780930df20d5cee21d047b13ef8ce4e39
Devin <devinpquinn4@gmail.com>
2020-09-03 18:19:19 -0400

    created basic npc script

---
+++ EOS-503/Assets/Devin/Scripts/NPC.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public string myName;
+    public RPGTalk myTalk;
+    public TextAsset myLines;
+
+    public void StartConversation()
+    {
+        myTalk.speakerName = myName;
+        myTalk.NewTalk("helloStart", "helloEnd");
+    }
+}
