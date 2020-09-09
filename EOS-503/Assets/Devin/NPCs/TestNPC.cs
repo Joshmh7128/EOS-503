d4ee06fc613d243eb4030f62f75db40244d445c8
Devin <devinpquinn4@gmail.com>
2020-09-09 11:31:13 -0400

    added player expressions and answer reactions

--- EOS-503/Assets/Devin/NPCs/TestNPC.cs
+++ EOS-503/Assets/Devin/NPCs/TestNPC.cs
@@ -24,56 +24,68 @@ public class TestNPC : NPC
                 if(num == 0)
                 {
                     myTalk.NewTalk("fail-a_Start", "fail-a_End");
+                    myTalk.PlayerPhoto.sprite = myTalk.sprites[2].sprite;
                 }
                 else if (num == 1)
                 {
                     myTalk.NewTalk("fail-b_Start", "fail-b_End");
+                    myTalk.PlayerPhoto.sprite = myTalk.sprites[2].sprite;
                 }
                 else if (num == 2)
                 {
                     myTalk.NewTalk("success_Start", "success_End");
+                    myTalk.PlayerPhoto.sprite = myTalk.sprites[1].sprite;
                 }
                 break;
             case "success":
                 if (num == 0)
                 {
                     myTalk.NewTalk("success-success_Start", "success-success_End");
+                    myTalk.PlayerPhoto.sprite = myTalk.sprites[1].sprite;
                 }
                 else if (num == 1)
                 {
                     myTalk.NewTalk("success-fail-a_Start", "success-fail-a_End");
+                    myTalk.PlayerPhoto.sprite = myTalk.sprites[2].sprite;
                 }
                 else if (num == 2)
                 {
                     myTalk.NewTalk("success-fail-b_Start", "success-fail-b_End");
+                    myTalk.PlayerPhoto.sprite = myTalk.sprites[2].sprite;
                 }
                 break;
             case "fail":
                 if (num == 0)
                 {
                     myTalk.NewTalk("fail-success_Start", "fail-success_End");
+                    myTalk.PlayerPhoto.sprite = myTalk.sprites[1].sprite;
                 }
                 else if (num == 1)
                 {
                     myTalk.NewTalk("fail-fail_Start", "fail-fail_End");
+                    myTalk.PlayerPhoto.sprite = myTalk.sprites[2].sprite;
                 }
                 else if (num == 2)
                 {
                     myTalk.NewTalk("fail-fail_Start", "fail-fail_End");
+                    myTalk.PlayerPhoto.sprite = myTalk.sprites[2].sprite;
                 }
                 break;
             case "success-fail":
                 if (num == 0)
                 {
                     myTalk.NewTalk("success-success_Start", "success-success_End");
+                    myTalk.PlayerPhoto.sprite = myTalk.sprites[1].sprite;
                 }
                 else if (num == 1)
                 {
                     myTalk.NewTalk("fail-fail_Start", "fail-fail_End");
+                    myTalk.PlayerPhoto.sprite = myTalk.sprites[2].sprite;
                 }
                 else if (num == 2)
                 {
                     myTalk.NewTalk("fail-fail_Start", "fail-fail_End");
+                    myTalk.PlayerPhoto.sprite = myTalk.sprites[2].sprite;
                 }
                 break;
         }
