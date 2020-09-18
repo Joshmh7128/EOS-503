94fac37bcc250ac867effabc88c8fe01637e94ae
Josh <joshmh7128@gmail.com>
2020-09-18 18:44:44 -0400

    Added Whitebox for scale testing, Minimap, adjusted player settings

---
+++ EOS-503/Assets/Josh/Scripts/PlayerController2D.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+    // control variables
+    [SerializeField] private Rigidbody2D playerRigidbody;
+    [SerializeField] private float playerSpeed = 5;
+    [SerializeField] private Transform playerCamTrans; // the transform of the actual camera
+    public CameraLerpScript cameraLerpScript; // so we can access it and give it camera positions when needed
+    public bool isRealPlayer;
+
+    // the awake function is called before anything else
+    private void Awake()
+    {
+        // make sure we don't lose the player
+        DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+        // check to see if player is the real player, if not, destroy it
+        if (isRealPlayer == false)
+        {
+            PlayerController otherPlayerController = GameObject.Find("Player Controller Package").GetComponent<PlayerController>();
+
+            if (otherPlayerController.isRealPlayer == true)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    // Fixed Update is called 60 times per second
+    void FixedUpdate()
+    {
+        /*
+        if (isRealPlayer == false)
+        {
+            PlayerController otherPlayerController = GameObject.Find("Player Controller Package").GetComponent<PlayerController>();
+
+            if (otherPlayerController.isRealPlayer == true)
+            {
+                Destroy(gameObject);
+            }
+        }   */
+
+        // movement check
+        PlayerMove();
+    }
+
+    // player movement
+    void PlayerMove()
+    {
+        // get horizontal and vertical movement
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        // set the velocity
+        playerRigidbody.velocity = new Vector3(horizontalMovement * playerSpeed, verticalMovement * playerSpeed);
+    }
+}
