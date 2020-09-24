e8b3489156223122cff92392b987219438f4aa4a
Devin <devinpquinn4@gmail.com>
2020-09-23 22:58:09 -0400

    first conversation setup

---
+++ EOS-503/Assets/Devin/NPCs/Argument01.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Argument01 : NPC
+{
+    public override void StartConversation()
+    {
+        myTalk.txtToParse = myLines[0];
+        myTalk.showPlayerPhoto = false;
+        base.StartConversation();
+    }
+}
