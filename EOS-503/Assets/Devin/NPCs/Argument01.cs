2641b712c4a2697c2ed75009aa53888a593bec95
Devin <devinpquinn4@gmail.com>
2020-09-24 13:28:00 -0400

    basic cutscene framework setup

--- EOS-503/Assets/Devin/NPCs/Argument01.cs
+++ EOS-503/Assets/Devin/NPCs/Argument01.cs
@@ -1,13 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Argument01 : NPC
 {
+    public UnityEvent OnJostle;
+    public GameObject jostleObj;
+
     public override void StartConversation()
     {
         myTalk.txtToParse = myLines[0];
         myTalk.showPlayerPhoto = false;
+        myTalk.callback = OnJostle;
         base.StartConversation();
+    }
+
+    public void Jostle()
+    {
+        Debug.Log("jostling");
+        myTalk.callback = null;
+        StartCoroutine(DoJostle());
+    }
+
+    IEnumerator DoJostle()
+    {
+        jostleObj.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        jostleObj.SetActive(false);
+        myTalk.NewTalk("blame-start", "blame-end");
     }
 }
