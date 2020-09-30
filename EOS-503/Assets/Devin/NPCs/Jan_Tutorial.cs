f4dd6c63c0986f83da92adbfa951e1fa49d1c8cd
Devin <devinpquinn4@gmail.com>
2020-09-30 18:03:41 -0400

    tutorial popup framework

--- EOS-503/Assets/Devin/NPCs/Jan_Tutorial.cs
+++ EOS-503/Assets/Devin/NPCs/Jan_Tutorial.cs
@@ -19,7 +19,32 @@ public class Jan_Tutorial : NPC
     public override void StartConversation()
     {
         myTalk.txtToParse = myLines[0];
+        myTalk.OnEndAnimating += OnEndAnimating; ///remember to clear on conversation end!
         base.StartConversation();
+    }
+
+    public void OnEndAnimating()
+    {
+        if(myTalk.rpgtalkElements[myTalk.cutscenePosition - 1].dialogText == "Perhaps no one thought to tell you, 'fixer', but about a year ago, trade between the cities turned sour. Being a clever businessman, I staved off the worst outcomes for myself, but I need new patrons to keep products moving or my business will fail.")
+        {
+            //NE intro tutorial popup
+        }
+        else if (myTalk.rpgtalkElements[myTalk.cutscenePosition - 1].dialogText == "And now you, from the High Council, you have come down to help? I have been around long enough to not trust in a savior to solve my troubles. What say you to that, esteemed DATA Agent? Hm?")
+        {
+            //first choice tutorial popup
+        }
+        else if (myTalk.rpgtalkElements[myTalk.cutscenePosition - 1].dialogText == "My mind is like a steel trap, but even with all of my memories to rely on, I am still stuck. I have been around long enough to not trust in a savior to solve my troubles. What say you to that, esteemed DATA Agent? Hm?")
+        {
+            //second choice tutorial popup
+        }
+        else if (myTalk.rpgtalkElements[myTalk.cutscenePosition - 1].dialogText == "This... is not a terrible idea, and fits with my memories. Maybe I am being too harsh in my judgment...? Hmmmm. ")
+        {
+            //win state tutorial popup
+        }
+        else if (myTalk.rpgtalkElements[myTalk.cutscenePosition - 1].dialogText == "Hmph, this is all you can think of to help me?")
+        {
+            //lose state tutorial popup
+        }
     }
 
     public override void OnMadeChoice(string id, int num)
