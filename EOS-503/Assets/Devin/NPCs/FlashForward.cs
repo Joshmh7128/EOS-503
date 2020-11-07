72647d73f12f28443cdb15e07e390de3af3550ae
Devin <devinpquinn4@gmail.com>
2020-11-06 22:27:52 -0500

    beginning convo setup

--- EOS-503/Assets/Devin/NPCs/FlashForward.cs
+++ EOS-503/Assets/Devin/NPCs/FlashForward.cs
@@ -12,6 +12,16 @@ public class FlashForward : NPC
     public override void StartConversation()
     {
         myTalk.txtToParse = myLines[0];
+        myTalk.OnEndAnimating += OnEndAnimating;
         base.StartConversation();
+    }
+
+    public void OnEndAnimating()
+    {
+        if (myTalk.rpgtalkElements[myTalk.cutscenePosition - 1].dialogText == "Thank you. That will end our inquiry.")
+        {
+            myTalk.PlayerPhoto.enabled = false;
+            myTalk.showPlayerPhoto = false;
+        }
     }
 }
