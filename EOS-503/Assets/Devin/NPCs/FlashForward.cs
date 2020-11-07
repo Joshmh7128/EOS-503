5a25ce11f456135214dc3dae1e9cd70ddbbe1cea
Devin <devinpquinn4@gmail.com>
2020-11-06 21:33:39 -0500

    intro building scene creation

---
+++ EOS-503/Assets/Devin/NPCs/FlashForward.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashForward : NPC
+{
+    private void Start()
+    {
+        myName = "???";
+    }
+
+    public override void StartConversation()
+    {
+        myTalk.txtToParse = myLines[0];
+        base.StartConversation();
+    }
+}
