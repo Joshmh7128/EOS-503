40a2f7d2cbb1f1612189cb12718cf7c1d33e78d4
Josh <joshmh7128@gmail.com>
2020-11-20 01:12:10 -0500

    Added NPC Navigation

---
+++ EOS-503/Assets/Josh/Scripts/Characters/NPCActiveNavigation.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCActiveNavigation : MonoBehaviour
+{
+    /// <summary>
+    /// this script spawns NPCs at a rate, and has them follow a predetermined path throughout the environment
+    /// </summary>
+
+    // the nodes that we will be working with to make our NPCs move
+    [SerializeField] Vector2[] PathNodes;
+    [SerializeField] Vector2[] PathNodesR; // random list
+    // which node are we on?
+    [SerializeField] int currentNode;
+    // what is our current destination to move to?
+    [SerializeField] Vector2 targetPosition;
+    // the radius of randomness applied to each node to make the NPCs look more natural
+    [SerializeField] float radiusRandomness;
+    // movement speed
+    [SerializeField] float movementSpeedlow;
+    [SerializeField] float movementSpeedhigh;
+    [SerializeField] float movementSpeedlocal;
+    // the NPC prefab we'll be using for the path finding
+    [SerializeField] GameObject pathwalkerNPC;
+    // variable to  hold the NPC we spawn
+    [SerializeField] GameObject activeNPC;
+
+    // start runs when the object 
+    private void Start()
+    {
+        // instantiate the walking NPC
+        activeNPC = Instantiate(pathwalkerNPC, transform);
+        // copy our list to our randomized list
+        PathNodesR = PathNodes;
+        // local counter
+        int i = 0;
+        // randomize our random list
+        foreach (Vector2 position in PathNodesR)
+        {
+            // make sure to round so that it doesn't fuck itself up
+           // PathNodesR[i] = new Vector2(Mathf.Round(position.x+Random.Range(-radiusRandomness, radiusRandomness)), Mathf.Round(position.y + Random.Range(-radiusRandomness, radiusRandomness)));
+            i += 1;
+        }
+        // make our first target position the first position in the coordinates list
+        targetPosition = PathNodesR[0];
+        // set our current node to 0
+        currentNode = 0;
+        // define our movement speed and truncate
+        movementSpeedlocal = Mathf.Round(Random.Range(movementSpeedlow, movementSpeedhigh)*100 ) /100f;
+    }
+
+    // fixed update runs once per frame
+    private void FixedUpdate()
+    {
+        // singleton to ensure we do not double count on frames when reaching destination
+        bool canAdd;
+        canAdd = false;
+        // every frame, move to our NPC to the target position
+        if (activeNPC != null)
+        {
+            // movement
+            activeNPC.transform.position = Vector2.MoveTowards(activeNPC.transform.position, targetPosition,  movementSpeedlocal);
+        }
+        // check to see if we have reached out target position, if so, add one to the node counter
+        if ((activeNPC.transform.position.x == targetPosition.x) && (activeNPC.transform.position.y == targetPosition.y))
+        {
+            // check singleton
+            canAdd = true;
+            // add one to the counter
+            if (canAdd == true)
+            {
+                currentNode += 1;
+                // perform a check
+                if (currentNode >= PathNodes.Length)
+                {
+                    ResetPathwalker();
+                }
+
+                // assign new target
+                targetPosition = PathNodesR[currentNode];
+                canAdd = false;
+            }
+        }
+
+        // when we reach the end of our path, reset the node counter and reinstatiate the NPC
+        if (currentNode >= PathNodes.Length)
+        {
+            ResetPathwalker();
+        }
+    }
+
+    // pathwalking 
+    private void ResetPathwalker()
+    {
+        // destroy our current NPC
+        Destroy(activeNPC);
+        // reset our counter
+        currentNode = 0;
+        // copy our list to our randomized list
+        PathNodesR = PathNodes;
+        // local counter
+        int i = 0;
+        // randomize our random list
+        foreach (Vector2 position in PathNodesR)
+        {
+            PathNodesR[i] = new Vector2(position.x + Mathf.Round(Random.Range(-radiusRandomness, radiusRandomness)), position.y + Mathf.Round(Random.Range(-radiusRandomness, radiusRandomness)));
+            i += 1;
+        }
+        // spawn our new NPC
+        activeNPC = Instantiate(pathwalkerNPC, transform);
+    }
+
+    // draw gizmos
+    private void OnDrawGizmos()
+    {
+        // draw each position in the path
+        foreach (Vector2 position in PathNodes)
+        {
+            Gizmos.DrawWireSphere(position, radiusRandomness);
+        }
+    }
+}
