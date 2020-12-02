baf368148b78b22aa74a7a1882ac8b1e8383aca0
Zachary Anderson <studiousstudent2019@gmail.com>
2020-12-01 21:12:45 -0500

    Door
    Am commit door.
    Amazing.

---
+++ EOS-503/Assets/Zack/Scripts/Argument02.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Animations;
+
+public class Argument02 : NPC
+{
+    private UnityEvent OnJostle;
+    private UnityEvent OnMinSuExit;
+    public Animator cutsceneAnim;
+    public Jan_Tutorial myJan;
+    public Hamadi_VS myHamadi;
+    public GameObject myPlayer;
+    public GameObject cutsceneCamera;
+    public GameObject damage;
+
+    private void Start()
+    {
+        OnJostle = new UnityEvent();
+        OnMinSuExit = new UnityEvent();
+        OnJostle.AddListener(Jostle);
+        OnMinSuExit.AddListener(MinSuExit);
+        StartCoroutine(EnterScene());
+    }
+
+    public override void StartConversation()
+    {
+        myTalk.txtToParse = myLines[0];
+        myTalk.showPlayerPhoto = false;
+        myTalk.callback = OnJostle;
+        base.StartConversation();
+    }
+
+    public void Jostle()
+    {
+        // myTalk.callback = OnFactionsEnter;
+        myTalk.callback = OnMinSuExit;
+        StartCoroutine(DoJostle());
+    }
+
+    public void MinSuExit()
+    {
+        myTalk.callback = null;
+        StartCoroutine(DoMinSuExit());
+    }
+
+    IEnumerator EnterScene()
+    {
+        yield return new WaitForSeconds(0f);
+        StartConversation();
+    }
+
+    IEnumerator DoJostle()
+    {
+        yield return new WaitForSeconds(0f);
+        damage.SetActive(true);
+        myTalk.showPlayerPhoto = false;
+        myTalk.NewTalk("hamadi-exit-start", "hamadi-exit-end");
+    }
+
+    IEnumerator DoMinSuExit()
+    {
+        yield return new WaitForSeconds(0f);
+        cutsceneCamera.GetComponent<Animator>().Play("cutsceneEnd");
+        yield return new WaitForSeconds(0f);
+        cutsceneAnim.gameObject.SetActive(false);
+        cutsceneCamera.SetActive(false);
+        myPlayer.SetActive(true);
+        //return control to player
+    }
+}
---
+++ EOS-503/Assets/Zack/Scripts/DoorSceneTransition.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorSceneTransition : MonoBehaviour
+{
+    [SerializeField] private string scene;
+    [SerializeField] private bool sceneTransition = false;
+    private bool interactable;
+
+    public BoxCollider2D DoorBox;
+
+    public Animator DoorL;
+    public Animator DoorR;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            interactable = true;
+            Debug.Log("very nice");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            interactable = false;
+
+            DoorBox.enabled = true;
+            if (DoorL) DoorL.SetBool("isOpen", false);
+            if (DoorR) DoorR.SetBool("isOpen", false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E) && interactable)
+        {
+            DoorBox.enabled = false;
+            if (DoorL) DoorL.SetBool("isOpen", true);
+            if (DoorR) DoorR.SetBool("isOpen", true);
+
+            if (sceneTransition) StartCoroutine(SceneTransition());
+        }
+    }
+
+    private IEnumerator SceneTransition()
+    {
+        Debug.Log("time to transition!");
+
+        yield return new WaitForSeconds(2f);
+
+        SceneManager.LoadScene(scene);
+    }
+}
