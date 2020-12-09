76ba48044b0466736f35a859a01dc0e8a8cc5b79
Devin <devinpquinn4@gmail.com>
2020-12-08 22:56:54 -0500

    added busra template

---
+++ EOS-503/Assets/Devin/NPCs/Busra_NE.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Busra_NE : NPC
+{
+    private void Start()
+    {
+        myName = "Busra Unal";
+    }
+
+    public override void StartConversation()
+    {
+        myTalk.txtToParse = myLines[0];
+        base.StartConversation();
+    }
+}
