b34357d8936f30f5b7879153d7fc2c9512bb5e10
Devin <devinpquinn4@gmail.com>
2021-01-10 14:45:48 -0500

    added thandiwe buhle template

---
+++ EOS-503/Assets/Devin/NPCs/Thandiwe_NE.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thandiwe_NE : NPC
+{
+    private void Start()
+    {
+        myName = "Thandiwe Buhle";
+    }
+
+    public override void StartConversation()
+    {
+        myTalk.txtToParse = myLines[0];
+        base.StartConversation();
+    }
+}
