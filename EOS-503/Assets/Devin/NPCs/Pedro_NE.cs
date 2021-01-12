46cbb09cc81939195bc4dfd4ef6bb2470016487e
Devin <devinpquinn4@gmail.com>
2021-01-12 17:34:05 -0500

    added People NE templates

---
+++ EOS-503/Assets/Devin/NPCs/Pedro_NE.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pedro_NE : NPC
+{
+    private void Start()
+    {
+        myName = "Pedro Reis Vidal";
+    }
+
+    public override void StartConversation()
+    {
+        myTalk.txtToParse = myLines[0];
+        base.StartConversation();
+    }
+}
