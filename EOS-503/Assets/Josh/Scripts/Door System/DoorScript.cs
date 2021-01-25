831ac0a08bbed7a4e3f0e75c3686cbe6af27e460
Josh <joshmh7128@gmail.com>
2021-01-25 12:58:12 -0500

    Player Controller Update

---
+++ EOS-503/Assets/Josh/Scripts/Door System/DoorScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    // variables
+    [SerializeField] GameObject playerObject; // get our player
+    [SerializeField] string targetScene; // which scene are we going to?
+    [SerializeField] Vector2 targetCoords; // what coordinates are we landing at?
+    [SerializeField] bool playerContact; // are we touching the player right now?
+
+    // Update is called once per frame
+    void Update()
+    {
+        // can we change rooms?
+        if (playerContact == true)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+
+            }
+        }
+    }
+
+    // use on trigger enter to see if we are touching the player
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        
+    }
+
+}
