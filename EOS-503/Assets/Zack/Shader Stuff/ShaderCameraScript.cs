c7b01a5616721685a536dd2c87ca78640aff7e05
Zachary Anderson <studiousstudent2019@gmail.com>
2021-02-20 20:06:12 -0500

    Baby Highlight Shader
    Basically imported the one from Scrapshoot. Gonna work on it more later.

---
+++ EOS-503/Assets/Zack/Shader Stuff/ShaderCameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ShaderCameraScript : MonoBehaviour
+{
+    // public RenderTexture texture;
+    public float multiplier = 10;
+    //public Material effectMaterial;
+    private Material effectMaterial;
+
+    private void Awake()
+    {
+        effectMaterial = new Material(Shader.Find("Hidden/HighlightShader"));
+        effectMaterial.SetFloat("_Multiplier", multiplier);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, effectMaterial);
+    }
+}
