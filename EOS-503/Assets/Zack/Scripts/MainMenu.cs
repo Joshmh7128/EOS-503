4a56d3ae512408d8d9dccd26e159d24234c30eae
Zachary Anderson <studiousstudent2019@gmail.com>
2021-02-20 22:31:07 -0500

    Main Menu!!!!!!
    Besides changing volume (because I don't know how the volume works in this game) the thing is done. If you've got questions about the scene management, hit me up.

---
+++ EOS-503/Assets/Zack/Scripts/MainMenu.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    // put a reference to audio
+
+    public Toggle fullscreen;
+    public Dropdown qualityDropdown;
+    public Dropdown resolutionDropdown;
+    public Dropdown sceneDropdown;
+
+    Resolution[] resolutions;
+
+    private int currentScene = 2;
+
+    private void Start()
+    {
+        fullscreen.isOn = Screen.fullScreen;
+
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " × " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        qualityDropdown.value = QualitySettings.GetQualityLevel();
+        qualityDropdown.RefreshShownValue();
+
+        sceneDropdown.value = currentScene - 1;
+        sceneDropdown.RefreshShownValue();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(currentScene);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        // Do audio shit here
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetScene(int sceneIndex)
+    {
+        currentScene = sceneIndex + 1;
+    }
+}
