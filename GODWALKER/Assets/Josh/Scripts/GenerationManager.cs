b2d5a9ff1a7a4943e68c206d77cc989d03ac8e1c
Josh <joshmh7128@gmail.com>
2021-03-02 15:21:57 -0500

    Unity be crazy sometimes.

--- GODWALKER/Assets/Josh/Scripts/GenerationManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GenerationManager : MonoBehaviour
-{
-    public int maxRoomSpawn;
-    public int activeRooms;
-}
