1247a2763703a320eca1b6283580f9e3620301b8
Zachary Anderson <studiousstudent2019@gmail.com>
2021-03-08 15:40:31 -0500

    Temp Push
    Miscommunications can be real bitches sometimes.
    At least now I know what Josh means by "Highlight Shader".

---
+++ EOS-503/Assets/Zack/Scripts/SpritePackingTestScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpritePackingTestScript : MonoBehaviour
+{
+    public Image m_DisplayImage;
+    public Sprite m_Sprite1, m_Sprite2;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(CombineSpritesCoroutine());
+        }
+    }
+
+    private IEnumerator CombineSpritesCoroutine()
+    {
+        Sprite[] spritesToCombine = new Sprite[2];
+
+        for (int x = 0; x < spritesToCombine.Length; x++)
+        {
+            spritesToCombine[x] = (x % 2 == 0 ? m_Sprite1 : m_Sprite2);
+        }
+
+        Sprite finalSprite = null;
+        yield return finalSprite = CombineSpriteArray(spritesToCombine);
+
+        m_DisplayImage.sprite = finalSprite;
+    }
+
+	public Sprite CombineSpriteArray(Sprite[] spritesArray)
+	{
+		int spritesWidth = (int)spritesArray[0].rect.width;
+		int spritesHeight = (int)spritesArray[0].rect.height;
+
+		Texture2D combinedTexture = new Texture2D(spritesWidth/* * spritesArray.Length*/, spritesHeight);
+
+		for (int x = 0; x < spritesArray.Length; x++)
+		{
+			combinedTexture.SetPixels(0/*x * spritesWidth*/, 0, spritesWidth, spritesHeight, spritesArray[x].texture.GetPixels());
+		}
+		combinedTexture.Apply();
+
+		return Sprite.Create(combinedTexture, new Rect(0.0f, 0.0f, combinedTexture.width, combinedTexture.height), new Vector2(0.5f, 0.5f), 100.0f);
+		}
+}
